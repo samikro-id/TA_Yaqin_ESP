409762faeee178fc1b0f5cc783d2eb6603502b96
samikro-id <samikro.id@gmail.com>
2022-12-18 03:28:03 +0700

    fix mqtt

--- TA_Yaqin_ESP.ino
+++ TA_Yaqin_ESP.ino
@@ -1,122 +1,31 @@
-/* Comment this out to disable prints and save space */
-#define BLYNK_PRINT Serial
-
-/* Fill-in your Template ID (only if using Blynk.Cloud) */
-#define BLYNK_TEMPLATE_ID "TMPL8hlT4cH5"
-#define BLYNK_DEVICE_NAME "Yaqin"
-#define BLYNK_AUTH_TOKEN "VQGAQp6knBhPPubx6M9FTZZqWluGFAmv"
-
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
+#include <ESP8266WiFi.h>                  // Install esp8266 by ESP8266 Community version 2.7.1
 #include <SoftwareSerial.h>
+#include <PubSubClient.h>                 // Install Library by Nick O'Leary version 2.7.0
 
 SoftwareSerial SerialSoft(14, 12);    // RX, TX
-
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = BLYNK_AUTH_TOKEN;
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 char ssid[] = "RND_Wifi";
 char pass[] = "RND12345";
 
-BlynkTimer timer;
+#define MQTT_ID         "7912c119-3a3c-4127-ac2e-440aadd221218"
 
-WidgetLED ledR(V3);
-WidgetLED ledY(V4);
-WidgetLED ledG(V5);
+#define MQTT_BROKER     "broker.emqx.io"            //
+#define MQTT_PORT       1883                        //
+#define MQTT_USERNAME   ""                          // Change to your Username from Broker
+#define MQTT_PASSWORD   ""                          // Change to your password from Broker
+#define MQTT_TIMEOUT    10
+#define MQTT_QOS        0
+#define MQTT_RETAIN    false
 
 uint32_t timer_serial;
+uint32_t led_time;
 bool has_data;
 
 #define SERIAL_LEN   100
-char text[SERIAL_LEN];
-
-void myTimerEvent()
-{
-  // String my_s = "UNO01000200001AMAN";
-  String my_s = "";
-
-  do{
-    if(SerialSoft.available() > 0){
-      char d = (char) SerialSoft.read();
-      timer_serial = millis();
-    }
-  }while((millis() - timer_serial) <= 100);
-
-  SerialSoft.println("DATA");
-  timer_serial = millis();  
-  do{
-    if(SerialSoft.available() > 0){
-//      delay(10);
-//      break;
-
-      char d = (char) SerialSoft.read();
-      if(d == '\n'){  
-        has_data = true; 
-        break;
-      }
-      else if(d == '\r'){ }
-      else{ my_s += d;  }
-
-      timer_serial = millis();
-    }
-  }while((millis() - timer_serial) <= 500);
-
-//  while(SerialSoft.available() > 0){
-//    char d = (char) SerialSoft.read();
-//    if(d == '\n'){  has_data = true; }
-//    else if(d == '\r'){ }
-//    else{ 
-//        my_s += d;
-//    }
-//    
-//    delay(1);
-//  }
-
-  if(has_data){
-    has_data = false;
-
-    // "UNO01000200001AMAN"
-    Serial.println(my_s);
-    String head = my_s.substring(0,3);
-    if(head == "UNO"){
-      String jarak_depan = my_s.substring(3, 7);
-      String jarak_belakang = my_s.substring(7, 11);
-      String led_r = my_s.substring(11, 12);
-      String led_y = my_s.substring(12, 13);
-      String led_g = my_s.substring(13,14);
-      String stat = my_s.substring(14);
-  
-      Serial.printf("depan %s\r\n", jarak_depan);
-      Serial.printf("belakang %s\r\n", jarak_belakang);
-      Serial.printf("R %s, Y %s, G %s\r\n", led_r, led_y, led_g);
-      Serial.printf("Stat %s\r\n", stat);
-  
-      // You can send any value at any time.
-      // Please don't send more that 10 values per second.
-      Blynk.virtualWrite(V1, jarak_depan.toInt());
-      Blynk.virtualWrite(V2, jarak_belakang.toInt());
-      
-      if(led_r.toInt()) { ledR.on(); }
-      else { ledR.off(); }
-  
-      if(led_y.toInt()) { ledY.on(); }
-      else { ledY.off(); }
-  
-      if(led_g.toInt()) { ledG.on(); }
-      else { ledG.off(); }
-      
-      Blynk.virtualWrite(V6, stat);   
-    }
-  }
-  else{
-    Serial.println("No Resp");
-  }
-
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-}
 
 void setup()
 {
@@ -131,16 +40,91 @@ void setup()
   // LED Built In Indicator
   pinMode(LED_BUILTIN, OUTPUT);
 
-  // Blynk.begin(auth, ssid, pass);
-  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_STA);                            delay(10);
+  ESP.wdtFeed();                                  yield();
 
   Serial.println("Start");
-  // Setup a function to be called every second
-  timer.setInterval(2000L, myTimerEvent);
+  timer_serial = millis();
 }
 
 void loop()
 {
-  Blynk.run();
-  timer.run(); // Initiates BlynkTimer
+  if(WiFi.status() == WL_CONNECTED){
+    if(client.connected()){
+      client.loop();
+
+      if((millis() - timer_serial) > 2000){
+        timer_serial = millis();
+
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+
+        // String my_s = "UNO01000200001AMAN";
+        String my_s = "";
+
+        do{
+          if(SerialSoft.available() > 0){
+            char d = (char) SerialSoft.read();
+            timer_serial = millis();
+          }
+        }while((millis() - timer_serial) <= 100);
+
+        SerialSoft.println("DATA");
+        timer_serial = millis();  
+        do{
+          if(SerialSoft.available() > 0){
+            char d = (char) SerialSoft.read();
+            if(d == '\n'){  
+              has_data = true; 
+              break;
+            }
+            else if(d == '\r'){ }
+            else{ my_s += d;  }
+
+            timer_serial = millis();
+          }
+        }while((millis() - timer_serial) <= 500);
+
+        if(has_data){
+          has_data = false;
+
+          // "UNO01000200001AMAN"
+          Serial.println(my_s);
+          client.publish("samikro/data/project/4", my_s.c_str(),false);
+        }
+        else{
+          Serial.println("No Resp");
+        }
+      }
+    }
+    else{
+      client.disconnect();
+      client.setServer(MQTT_BROKER, MQTT_PORT);
+
+      uint8_t i;
+      for(i = 0; i < MQTT_TIMEOUT; i++) {
+          
+        if( client.connect(MQTT_ID, MQTT_USERNAME, MQTT_PASSWORD) ){
+          break;
+        }
+
+        delay(500);    
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));    // toggle led setiap 500ms                         
+      }
+    }
+  }
+  else{
+    WiFi.begin(ssid, pass);
+    while(WiFi.status() != WL_CONNECTED){
+      yield();
+
+      if((millis()-led_time) > 200){          // toggle led setiap 200ms
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+      }
+
+      if(WiFi.status() == WL_CONNECTED){
+        break;
+      }
+    }
+  }
 }
